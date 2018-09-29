60672014200099d1a9f0e5b1490d5565006b2444
tosy <tosy@163.com>
2018-09-29 13:43:47 +0800

    1

--- tosylabbot/src/main/java/com/tosylab/tosylabbot/HelloC.java
+++ tosylabbot/src/main/java/com/tosylab/tosylabbot/HelloC.java
@@ -1,18 +1,16 @@
 package com.tosylab.tosylabbot;
 
 import com.alibaba.fastjson.JSONObject;
-import com.google.common.collect.Maps;
+import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Enumeration;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
@@ -33,35 +31,41 @@ public class HelloC {
         ModelTelegramUpdate update = JSONObject.parseObject(body,ModelTelegramUpdate.class);
 //        logger.info("get body ... " + JSONObject.toJSONString(update));
 
-        StringBuilder builder = new StringBuilder(telegramUrl).append(telegramToken).append("/sendMessage?chat_id=672868707&text=hello");
+        //get
+//        StringBuilder builder = new StringBuilder(telegramUrl).append(telegramToken).append("/sendMessage?chat_id=672868707&text=hello");
 //        logger.info("send url = {}",builder.toString());
-        Unirest.get(builder.toString()).asJsonAsync();
+//        Unirest.get(builder.toString()).asJsonAsync();
 
-
+        //post
         StringBuilder builderPost = new StringBuilder(telegramUrl).append(telegramToken).append("/sendMessage");
-        ModelSendText sendText = new ModelSendText();
-        sendText.setChat_id(update.getMessage().getChat().getId());
-        sendText.setText(getDefaultText());
-        logger.info("body " + JSONObject.toJSONString(sendText));
-        Unirest.post(builderPost.toString()).body(JSONObject.toJSONString(sendText)).asJsonAsync();
+        ModelSendText hello = new ModelSendText();
+        hello.setChat_id(update.getMessage().getChat().getId());
+        hello.setText("hello " + update.getMessage().getFrom().getUsername());
+
+        logger.info("body " + JSONObject.toJSONString(hello));
+        Unirest.post(builderPost.toString())
+                .header("Content-Type", "application/json")
+                .body(JSONObject.toJSONString(hello)).asJsonAsync();
+
+        ModelSendText mainpage = new ModelSendText();
+        mainpage.setChat_id(update.getMessage().getChat().getId());
+        mainpage.setParse_mode("markdown");
+        mainpage.setText("[inline URL](https://www.tosylab.com)");
+
         return "OK";
     }
 
-    private String getDefaultText() {
-        return "hello2";
-//        return "[inline URL](https://www.tosylab.com)";
-    }
 
-    private Map<String,String> getParameters(HttpServletRequest request) {
-        Map<String, String> map = Maps.newHashMap();
-        Enumeration<String> paramNames = request.getParameterNames();
-        while (paramNames.hasMoreElements()) {
-            String paramName = paramNames.nextElement();
-            String[] paramValues = request.getParameterValues(paramName);
-            if (paramValues != null && paramValues.length > 0) {
-                map.put(paramName, paramValues[0]);
-            }
-        }
-        return map;
+    public static void main(String[] args) throws UnirestException {
+
+        StringBuilder builderPost = new StringBuilder("https://api.telegram.org/bot")
+                .append("580249700:AAGSAE0bewvzOMd4vZMCy3TkjkXxPTSYxvk").append("/sendMessage");
+        ModelSendText sendText = new ModelSendText();
+        sendText.setChat_id(672868707);
+        sendText.setText("hi");
+        HttpResponse<String> rt = Unirest.post(builderPost.toString())
+                .header("Content-Type", "application/json")
+                .body(JSONObject.toJSONString(sendText)).asString();
+        System.out.println("x" + JSONObject.toJSONString(rt));
     }
 }
--- tosylabbot/src/main/java/com/tosylab/tosylabbot/ModelSendText.java
+++ tosylabbot/src/main/java/com/tosylab/tosylabbot/ModelSendText.java
@@ -3,6 +3,15 @@ package com.tosylab.tosylabbot;
 public class ModelSendText {
     long chat_id;
     String text;
+    String parse_mode;          //markdown  html
+
+    public String getParse_mode() {
+        return parse_mode;
+    }
+
+    public void setParse_mode(String parse_mode) {
+        this.parse_mode = parse_mode;
+    }
 
     public long getChat_id() {
         return chat_id;
