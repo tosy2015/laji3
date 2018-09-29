5255d4c147bcec5e2d78e925dcac011b7284edfb
tosy <tosy@163.com>
2018-09-29 10:56:03 +0800

    1

--- tosylabbot/start.sh
+++ tosylabbot/start.sh
@@ -5,4 +5,4 @@ mvn clean
 mvn package
 #nohup java -server -XX:+UseG1GC -Xmx10m -Xms10m -jar /root/laji3/nettytest/target/nettytest-1.jar  >/dev/null 2>&1 &
 #nohup java -server -XX:+UseG1GC -Xmx10m -Xms10m -jar /root/laji3/nettytest/target/nettytest-1.jar  >/dev/null 2>log &
-nohup java -server -XX:+UseG1GC -Xmx10m -Xms10m -jar /root/laji3/tosylabbot/target/tosylabbot-1.0.jar
+nohup java -server -XX:+UseG1GC -Xmx10m -Xms10m -jar /root/laji3/tosylabbot/target/tosylabbot-1.0.jar >/dev/null 2>&1 &
