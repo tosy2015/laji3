a22398e472d368b7d88aa59ad6108a2d10954e67
tosy <tosy@163.com>
2018-09-29 11:46:28 +0800

    1

--- tosylabbot/start.sh
+++ tosylabbot/start.sh
@@ -1,7 +1,7 @@
 #!bash
 git pull
 ps aux | grep -i "tosylabbot" | awk '{print $2}' | xargs sudo kill
-mvn clean
+#mvn clean
 mvn package
 #nohup java -server -XX:+UseG1GC -Xmx10m -Xms10m -jar /root/laji3/nettytest/target/nettytest-1.jar  >/dev/null 2>&1 &
 #nohup java -server -XX:+UseG1GC -Xmx10m -Xms10m -jar /root/laji3/nettytest/target/nettytest-1.jar  >/dev/null 2>log &
